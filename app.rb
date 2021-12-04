b589bd9011a1819bf25dfa9eacc7f1a44265099c
Camilla <camilla.lima000@gmail.com>
2021-12-04 20:11:00 +0000

    Added update feature

--- app.rb
+++ app.rb
@@ -3,28 +3,33 @@ require_relative './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   enable :sessions, :method_override
-
   get '/' do
     'Bookmark Manager'
   end
-
   get '/bookmarks' do
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
   end
-
   get '/bookmarks/new' do
     erb :'bookmarks/new'
   end
-
   post '/bookmarks' do
     Bookmark.create(url: params[:url], title: params[:title])
     redirect '/bookmarks'
   end
-
   delete '/bookmarks/:id' do
     Bookmark.delete(id: params[:id])
     redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :"bookmarks/edit"
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect('/bookmarks')
   end
 
   run! if app_file == $0
