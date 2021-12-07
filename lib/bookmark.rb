2ed5cda944f929a707091cc40e1762e5d9c2e660
Camilla <camilla.lima000@gmail.com>
2021-12-07 13:20:23 +0000

    Many to Many Relationships - adding tags and comments

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,7 +1,7 @@
 require_relative 'database_connection'
 require 'uri'
 require_relative './comment'
-
+require_relative './tag'
 class Bookmark
   def self.all
     bookmarks = DatabaseConnection.query('SELECT * FROM bookmarks;')
@@ -37,19 +37,28 @@ class Bookmark
     )
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
+
+  def self.where(tag_id:)
+    result = DatabaseConnection.query("SELECT id, title, url FROM bookmarks_tags INNER JOIN bookmarks ON bookmarks.id = bookmarks_tags.bookmark_id WHERE bookmarks_tags.tag_id = '#{tag_id}';")
+    result.map do |bookmark|
+      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+    end
+  end
+
   attr_reader :id, :title, :url
+
   def initialize(id:, title:, url:)
     @id = id
     @title = title
     @url = url
   end
-
   def comments(comment_class = Comment)
     comment_class.where(bookmark_id: id)
   end
-
+  def tags(tag_class = Tag)
+    tag_class.where(bookmark_id: id)
+  end
   private
-
   def self.is_url?(url)
     url =~ URI::DEFAULT_PARSER.regexp[:ABS_URI]
   end
